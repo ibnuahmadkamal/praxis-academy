c2b321a00a5d8d7b5a8657af0a8602e0696ad5e4
ibnuahmadkamal <ibnuahmadkamal13@gmailcom>
2019-12-02 16:11:29 +0700

    make it better

---
+++ kemampuan-dasar-1/kasus/cari-java.bat
@@ -0,0 +1,8 @@
+$cari = -Get-ChildItem -path C:\Users\Asus X550Z\Documents\Git *-include * .java
+if(cari = .java){
+	ada file;
+}
+else{
+	tidak ada file;
+}
+pause
---
+++ kemampuan-dasar-1/kasus/cari-java.ps1
@@ -0,0 +1,3 @@
+echo ada file java di:
+cd C:\Users\Asus X550Z\Documents\Git\praxis-academy\kemampuan-dasar-1\kasus .java
+pause
