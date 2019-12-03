0088b84c9475b033638a7beffa45f3d537bbc99e
ibnuahmadkamal <ibnuahmadkamal13@gmailcom>
2019-12-03 13:04:36 +0700

    make it better

---
+++ kemampuan-dasar-1/kasus/cari-java(fix).ps1
@@ -0,0 +1,4 @@
+@echo off
+echo ada file java di:
+attrib /s /d *.java
+pause
---
+++ kemampuan-dasar-1/kasus/file-ren.bat
@@ -0,0 +1,5 @@
+@echo off
+echo masukan nama file baru :
+cd C:\Users\Asus X550Z\Documents\Git\praxis-academy\kemampuan-dasar-1\kasus
+ren *.java
+pause
