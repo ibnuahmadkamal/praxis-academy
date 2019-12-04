d0590f22ad841fa6fafd3a9294ac2d86c5ed1131
ibnuahmadkamal <ibnuahmadkamal13@gmailcom>
2019-12-04 18:16:03 +0700

    make it better

---
+++ Novice/01.01/contoh materi input&output.py
@@ -0,0 +1,16 @@
+#3contoh input&output
+# 1. input & output
+nama = input("Siapa nama kamu: ")
+umur = input("Berapa umur kamu: ")
+
+print ("Hello",nama,"umur kamu adalah",umur,"tahun")
+
+#penerapan if
+angka1 = input("masukan angka pertama:")
+angka2 = input("masukan angka kedua: ")
+
+if angka1>angka2:
+    print ("angka pertama lebih besar dari angka kedua")
+else:
+    print ("angka pertama lebih kecil dari angka kedua")
+    
---
+++ Novice/01.01/contoh materi list.py
@@ -0,0 +1,42 @@
+print ("hallo")
+
+#l.append() = menambahkan di baris terakhir
+a = [1,2,3,4]
+a.append(5)
+print (a)
+
+#l.insert(i,j) = menambahkan di depan terakhir
+a = [1,2,3,4]
+a.insert(0,0)
+print (a)
+
+#l.remove() = menghapus index yg dipilih
+a = [1,2,3,4]
+a.remove(3)
+print (a)
+'''
+#l.extends
+a = [1,2,3,4]
+a.extend(5)
+print (a)
+'''
+#l.clear()  = menghapus seluruh isi list
+a = [1,2,3,4]
+a.clear()
+print (a)
+
+#l.count(x) = mencari index ke x
+a = [1,2,3,4]
+a.remove(3)
+print (a)
+
+#l.sort() = mengurutkan
+a = [2,1,3,4]
+a.sort()
+print (a)
+
+#l.reverse() = membalik list belakang -> depan
+a = [1,2,3,4]
+a.reverse()
+print (a)
+
