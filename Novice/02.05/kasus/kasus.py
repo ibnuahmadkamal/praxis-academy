9dc6e862c2f95d45fc2d94bfd6bcd989588a4b6c
ibnuahmadkamal <ibnuahmadkamal13@gmailcom>
2019-12-17 23:48:01 +0700

    make it better

--- Novice/02.05/kasus/kasus.py
+++ Novice/02.05/kasus/kasus.py
@@ -5,11 +5,13 @@ db = mysql.connector.connect(
   passwd="",
   database="kasus_praxis"
 )
+number = 0
 cursor = db.cursor()
-sql = "select movie_rented FROM tabel2 WHERE Membership_id = 1"
+sql = "SELECT t1.Full_Names, t2.movie_rented FROM tabel1 t1, tabel2 t2 WHERE t1.Membership_id = t2.membership_id and t1.Full_Names='janet jones'"
 cursor.execute(sql)
 
-results = cursor.fetchall()
-
-for data in results:
-  print(data)
+for Full_Names, movie_rented in cursor:
+    number += 1
+    # print(number, "{}rented {}".format(Full_Names, Movies_Rented))
+    print("{}".format(Full_Names))
+    print( number,"{}".format(movie_rented)) 
